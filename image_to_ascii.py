8285b202b3134df59c561fd86b563f55161c8f87
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-03-08 23:45:42 +0530

    Add files via upload

---
+++ image_to_ascii.py
@@ -0,0 +1,25 @@
+from PIL import ImageEnhance
+from PIL import Image
+import numpy
+
+im1 = Image.open(r"C:\Users\Kunj R. Patel\Desktop\python\image related\tiger.jpeg")
+enhancer = ImageEnhance.Color(im1)
+#im1 = enhancer.enhance(0)
+im1 = im1.resize((500,300))
+#im1 = im1.rotate(90)
+im1.show()
+
+chars = ["@","#","S","&","?","*","+",";",":",",","."]
+a = numpy.asarray(im1)
+count = 0
+
+with open(r".\tiger.txt", "w") as file:
+    for i in a:
+        for j in i:
+            sym = (chars[int(( j[0] + j[1] + j[2] ) / 76.5 )])
+            print(sym," ",count)
+            count += 1
+            file.write(sym)
+        file.write("\n")
+    file.close()    
+        
