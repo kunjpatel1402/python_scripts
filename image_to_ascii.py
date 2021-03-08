55ae3feb9de21ad278f5ca50db37dd50b1f6ebc3
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-03-08 23:46:54 +0530

    Update image_to_ascii.py

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -1,11 +1,11 @@
 from PIL import ImageEnhance
 from PIL import Image
 import numpy
-
-im1 = Image.open(r"C:\Users\Kunj R. Patel\Desktop\python\image related\tiger.jpeg")
+#converts image into ascii characters
+im1 = Image.open(r"path")
 enhancer = ImageEnhance.Color(im1)
 #im1 = enhancer.enhance(0)
-im1 = im1.resize((500,300))
+im1 = im1.resize((750,750))
 #im1 = im1.rotate(90)
 im1.show()
 
@@ -13,7 +13,7 @@ chars = ["@","#","S","&","?","*","+",";",":",",","."]
 a = numpy.asarray(im1)
 count = 0
 
-with open(r".\tiger.txt", "w") as file:
+with open(r".\image.txt", "w") as file:
     for i in a:
         for j in i:
             sym = (chars[int(( j[0] + j[1] + j[2] ) / 76.5 )])
