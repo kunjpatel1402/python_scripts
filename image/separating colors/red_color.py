acd6949dbaf591cb0d1f2c73e5ff5107d66f2af7
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-03-09 16:43:55 +0530

    Update red_color.py

--- image/separating colors/red_color.py
+++ image/separating colors/red_color.py
@@ -1,1 +1,28 @@
 
+from PIL import ImageEnhance
+from PIL import Image
+import numpy
+
+im1 = Image.open(r"path")
+
+im1 = im1.resize((500,500))
+im1.show()
+
+a = numpy.asarray(im1)
+row = 0
+pixels = numpy.ones((500,500,3))
+im2 = Image.new(im1.mode,(500,500))
+new_pixels = im2.load()
+
+for i in a:
+    col = 0
+    for j in i:
+        print (j," ", row*500 + col)
+        new_pixels[col,row] = (j[0],0,0)
+        print (new_pixels[row,col])
+        #print (pixels[row-1,col-1])
+        #print (row,col)
+        col += 1
+    row += 1
+
+im2.show()
